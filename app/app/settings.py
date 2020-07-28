32d750bcdcb88402afca5a803b97ac00acd6ecbf
Mateo <rosenmatt1@gmail.com>
2020-07-28 12:57:17 -0600

    Added allowed hosts

--- app/app/settings.py
+++ app/app/settings.py
@@ -17,9 +17,8 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
@@ -37,7 +36,7 @@ SECRET_KEY = os.environ.get('MUSIC_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['music-player-account.herokuapp.com', 'music-player-app-c715c.web.app']
 
 
 # Application definition
@@ -175,11 +174,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-# STATIC_URL = '/static/'
 
 # Activate Django_Heroku
 django_heroku.settings(locals())
