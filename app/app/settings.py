a53ffe607d048589b657a74dc6760e7456d52676
Mateo <rosenmatt1@gmail.com>
2020-08-02 00:02:28 -0600

    Deploying Heroku

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-# import django-cors-headers
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -32,11 +31,11 @@ STATICFILES_DIRS = (
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('MUSIC_KEY')
-# SECRET_KEY = '2+pxbp%8w9n3#+sp!*3%6vk$gt2fg2rl7@534w^-y7s!^soaoe'
+# SECRET_KEY = os.environ.get('MUSIC_KEY')
+SECRET_KEY = '2+pxbp%8w9n3#+sp!*3%6vk$gt2fg2rl7@534w^-y7s!^soaoe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['music-player-account.herokuapp.com', 'music-player-app-c715c.web.app', 'music-player-app-c715c.firebaseapp.com']
 ALLOWED_HOSTS = ['*']
